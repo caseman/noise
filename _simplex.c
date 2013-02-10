ddcf54c70f31c0e4b4abaa51467f48c83fc5fb5c
Casey Duncan <casey.duncan@gmail.com>
2013-02-10 13:21:08 -0700

    fix broken 3D simplex noise

--- _simplex.c
+++ _simplex.c
@@ -100,17 +100,17 @@ noise3(float x, float y, float z)
 	float k = floorf(z + s);
 	float t = (i + j + k) * G3;
 
-	float3 pos[3];
+	float pos[4][3];
 
-	pos[0].x = x - (i - t);
-	pos[0].y = y - (j - t);
-	pos[0].z = z - (k - t);
+	pos[0][0] = x - (i - t);
+	pos[0][1] = y - (j - t);
+	pos[0][2] = z - (k - t);
 
-	if (pos[0].x >= pos[0].y) {
-		if (pos[0].y >= pos[0].z) {
+	if (pos[0][0] >= pos[0][1]) {
+		if (pos[0][1] >= pos[0][2]) {
 			ASSIGN(o1, 1, 0, 0);
 			ASSIGN(o2, 1, 1, 0);
-		} else if (pos[0].x >= pos[0].z) {
+		} else if (pos[0][0] >= pos[0][2]) {
 			ASSIGN(o1, 1, 0, 0);
 			ASSIGN(o2, 1, 0, 1);
 		} else {
@@ -118,10 +118,10 @@ noise3(float x, float y, float z)
 			ASSIGN(o2, 1, 0, 1);
 		}
 	} else {
-		if (pos[0].x < pos[0].z) {
+		if (pos[0][0] < pos[0][2]) {
 			ASSIGN(o1, 0, 0, 1);
 			ASSIGN(o2, 0, 1, 1);
-		} else if (pos[0].x < pos[0].z) {
+		} else if (pos[0][0] < pos[0][2]) {
 			ASSIGN(o1, 0, 1, 0);
 			ASSIGN(o2, 0, 1, 1);
 		} else {
@@ -131,9 +131,9 @@ noise3(float x, float y, float z)
 	}
 	
 	for (c = 0; c <= 2; c++) {
-		pos[3].xyz[c] = pos[0].xyz[c] - 1.0f + 3.0f * G3;
-		pos[2].xyz[c] = pos[0].xyz[c] - o2[c] + 2.0f * G3;
-		pos[1].xyz[c] = pos[0].xyz[c] - o1[c] + G3;
+		pos[3][c] = pos[0][c] - 1.0f + 3.0f * G3;
+		pos[2][c] = pos[0][c] - o2[c] + 2.0f * G3;
+		pos[1][c] = pos[0][c] - o1[c] + G3;
 	}
 
 	I = (int) i & 255; 
@@ -144,12 +144,15 @@ noise3(float x, float y, float z)
 	g[2] = PERM[I + o2[0] + PERM[J + o2[1] + PERM[o2[2] + K]]] % 12;
 	g[3] = PERM[I + 1 + PERM[J + 1 + PERM[K + 1]]] % 12; 
 
-	for (c = 0; c <= 3; c++)
-		f[c] = 0.6f - pos[0].xyz[c]*pos[0].xyz[c] - pos[1].xyz[c]*pos[1].xyz[c] - pos[2].xyz[c]*pos[2].xyz[c];
+	for (c = 0; c <= 3; c++) {
+		f[c] = 0.6f - pos[c][0]*pos[c][0] - pos[c][1]*pos[c][1] - pos[c][2]*pos[c][2];
+	}
 	
-	for (c = 0; c <= 3; c++)
-		if (f[c] > 0)
-			noise[c] = f[c]*f[c]*f[c]*f[c] * dot3(pos[c].xyz, GRAD3[g[c]]);
+	for (c = 0; c <= 3; c++) {
+		if (f[c] > 0) {
+			noise[c] = f[c]*f[c]*f[c]*f[c] * dot3(pos[c], GRAD3[g[c]]);
+		}
+	}
 	
 	return (noise[0] + noise[1] + noise[2] + noise[3]) * 32.0f;
 }
