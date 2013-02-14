0f5bbd47361fc51bcf7f6a8182388b8a432d4442
Casey Duncan <casey.duncan@gmail.com>
2013-02-14 00:32:19 -0800

    Update 2dtexture example to work with python 3

--- examples/2dtexture.py
+++ examples/2dtexture.py
@@ -4,24 +4,24 @@
 # $Id: 2dtexture.py 21 2008-05-21 07:52:29Z casey.duncan $
 
 import sys
-from noise import pnoise2
+from noise import pnoise2, snoise2
 
 if len(sys.argv) not in (2, 3) or '--help' in sys.argv or '-h' in sys.argv:
-	print >>sys.stderr, '2dtexture.py FILE [OCTAVES]'
-	print >>sys.stderr
-	print >>sys.stderr, __doc__
+	print('2dtexture.py FILE [OCTAVES]')
+	print()
+	print(__doc__)
 	raise SystemExit
 
-f = open(sys.argv[1], 'wb')
+f = open(sys.argv[1], 'wt')
 if len(sys.argv) > 2:
 	octaves = int(sys.argv[2])
 else:
 	octaves = 1
 freq = 16.0 * octaves
-print >>f, 'P2'
-print >>f, '256 256'
-print >>f, '255'
+f.write('P2\n')
+f.write('256 256\n')
+f.write('255\n')
 for y in range(256):
 	for x in range(256):
-		print >>f, int(pnoise2(x / freq, y / freq, octaves) * 127.0 + 128.0)
+		f.write("%s\n" % int(snoise2(x / freq, y / freq, octaves) * 127.0 + 128.0))
 f.close()
